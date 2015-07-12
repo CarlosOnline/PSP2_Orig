55026d30359ca8315a6a97feb842a35fb7160092
CarlosOnline <cgomes@iinet.com>
2015-07-11 18:18:55 -0700

    yo aurelia step

--- git_setup_psp.cmd
+++ git_setup_psp.cmd
@@ -2,3 +2,4 @@ git remote add origin https://github.com/CarlosOnline/PSP2.git
 git config user.email cgomes@iinet.com
 git config user.name CarlosOnline
 git config credential.https://github.com.username CarlosOnline
+git pull -u origin master
---
+++ setup_aurelia.cmd
@@ -0,0 +1,6 @@
+rem call npm install -g gulp
+call npm install -g jspm
+call jspm registry config github
+call xcopy /fredhicky c:\temp\skeleton-navigation-0.15.1\
+call npm install -g yo generator-aurelia
+call yo aurelia
